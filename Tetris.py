75d4d0a44ed14a31cf24e377b5c8e37a3b6dcf95
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-05-26 17:23:37 +0600

    Updated

--- Tetris.py
+++ Tetris.py
@@ -163,6 +163,16 @@ def run():
 
         pygame.display.update()
 
+def create_game_matrix():
+    game_matrix_columns = 10
+    game_matrix_rows = 20
+    matrix = []
+    for row in range(game_matrix_rows):
+        new_row = []
+        for column in range(game_matrix_columns):
+            new_row.append('.')
+        matrix.append(new_row)
+    return matrix
 
 def create_piece():
     piece = {}
@@ -173,6 +183,20 @@ def create_piece():
     piece['column'] = 2
     return piece
 
+def draw_board(screen,matrix):
+    matrix_row = 20
+    matrix_column = 10
+    for row in range(matrix_row):
+        for col in range(matrix_column):
+            if(matrix[row][col] == 'c'):
+                draw_single_piece(screen,row,col,WHITE,GREY)
+
+def draw_single_piece(screen,row,column,color,color2):
+    origin_x = 100+5+(column*20+1)
+    origin_y = 50+5+(row*20+1)
+    pygame.draw.rect(screen,color,[origin_x,origin_y,20,20])
+    pygame.draw.rect(screen,color2,[origin_x,origin_y,18,18])
+
 def draw_big_piece(screen ,piece, color):
     shape_to_draw = availble_piece()[piece['shape']][piece['rotation']]
     for row in range(5):
@@ -180,41 +204,9 @@ def draw_big_piece(screen ,piece, color):
             if(shape_to_draw[row][col]=='c'):
                 draw_single_piece(screen,piece['row']+row,piece['column']+col,GREY,color)
 
-def update_matrix(matrix,piece):
-    for row in range(5):
-        for col in range(5):
-            if(availble_piece()[piece['shape']][piece['rotation']][row][col] == 'c'):
-                matrix[piece['row']+row][piece['column']+col] = 'c'
-    return matrix
 
-def valid_position(game_matrix,piece,adjc=0,adjr=0):
-    piece_matrix = availble_piece()[piece['shape']][piece['rotation']]
-    for row in range(5):
-        for col in range(5):
-            if(piece_matrix[row][col]=='.'):
-                continue
-            if(not inside_board(piece['row']+row+adjr,piece['column']+col+adjc)):
-                return False
-            if(game_matrix[piece['row']+row+adjr][piece['column']+col+adjc]=='c'):
-                return False
-    return True
-
-def show_score(screen,score):
-    myfont = pygame.font.Font('freesansbold.ttf',18)
-    text_surface = myfont.render('Score: %s' % score,True,WHITE)
-    screen.blit(text_surface,(350,20))
-
-def remove_line(game_matrix):
-    line_removed = 0
-    for row in range(20):
-        if(line_complete(game_matrix,row)):
-            for row_to_shift in range(row,0,-1):
-                for column in range(10):
-                    game_matrix[row_to_shift][column] = game_matrix[row_to_shift-1][column]
-            for i in range(10):
-                game_matrix[0][i] = '.'
-            line_removed += 1
-    return line_removed
+def inside_board(row,column):
+    return column >=0 and column < 10 and row < 20
 
 def line_complete(game_matrix,row):
     for column in range(10):
@@ -239,33 +231,42 @@ def listen_to_user_input(game_matrix,piece):
                      piece['rotation'] = (piece['rotation']-1) % len(availble_piece()[piece['shape']])
                 return
 
-def inside_board(row,column):
-    return column >=0 and column < 10 and row < 20
+def remove_line(game_matrix):
+    line_removed = 0
+    for row in range(20):
+        if(line_complete(game_matrix,row)):
+            for row_to_shift in range(row,0,-1):
+                for column in range(10):
+                    game_matrix[row_to_shift][column] = game_matrix[row_to_shift-1][column]
+            for i in range(10):
+                game_matrix[0][i] = '.'
+            line_removed += 1
+    return line_removed
 
-def draw_board(screen,matrix):
-    matrix_row = 20
-    matrix_column = 10
-    for row in range(matrix_row):
-        for col in range(matrix_column):
-            if(matrix[row][col] == 'c'):
-                draw_single_piece(screen,row,col,WHITE,GREY)
 
-def draw_single_piece(screen,row,column,color,color2):
-    origin_x = 100+5+(column*20+1)
-    origin_y = 50+5+(row*20+1)
-    pygame.draw.rect(screen,color,[origin_x,origin_y,20,20])
-    pygame.draw.rect(screen,color2,[origin_x,origin_y,18,18])
+def show_score(screen,score):
+    myfont = pygame.font.Font('freesansbold.ttf',18)
+    text_surface = myfont.render('Score: %s' % score,True,WHITE)
+    screen.blit(text_surface,(350,20))
 
-def create_game_matrix():
-    game_matrix_columns = 10
-    game_matrix_rows = 20
-    matrix = []
-    for row in range(game_matrix_rows):
-        new_row = []
-        for column in range(game_matrix_columns):
-            new_row.append('.')
-        matrix.append(new_row)
+def update_matrix(matrix,piece):
+    for row in range(5):
+        for col in range(5):
+            if(availble_piece()[piece['shape']][piece['rotation']][row][col] == 'c'):
+                matrix[piece['row']+row][piece['column']+col] = 'c'
     return matrix
+
+def valid_position(game_matrix,piece,adjc=0,adjr=0):
+    piece_matrix = availble_piece()[piece['shape']][piece['rotation']]
+    for row in range(5):
+        for col in range(5):
+            if(piece_matrix[row][col]=='.'):
+                continue
+            if(not inside_board(piece['row']+row+adjr,piece['column']+col+adjc)):
+                return False
+            if(game_matrix[piece['row']+row+adjr][piece['column']+col+adjc]=='c'):
+                return False
+    return True
 
 run()
 exit(0)
